57bf404d1c786220a6c601c5c1fc7d0511d0d2c0
Gabriel G. <ggoldstein771@gmail.com>
2023-09-26 17:44:42 -0700

    Added chat messages component, chat item component, and edit functionality.

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -44,6 +46,31 @@ const ChannelIdPage = async (
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">
+                <ChatMessages 
+                    member={member}
+                    name={channel.name}
+                    chatId={channel.id}
+                    type="channel"
+                    apiUrl="/api/messages"
+                    socketUrl="/api/socket/messages"
+                    socketQuery={{
+                        channelId: channel.id,
+                        serverId: channel.serverId
+                    }}
+                    paramKey="channelId"
+                    paramValue={channel.id}
+                />
+            </div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }}
             />
         </div>
     );
